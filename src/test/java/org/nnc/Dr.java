a42f8965b983740c412505c5dacab8a8840adaec
manitom <tms.manimaran@gmail.com>
2022-08-24 23:50:57 -0700

    1st time push

---
+++ src/test/java/org/nnc/Dr.java
@@ -0,0 +1,20 @@
+package org.nnc;
+
+import java.lang.reflect.Method;
+
+public class Dr {
+	private void method() {
+		System.out.println("1");
+
+	}
+	private void method1() {
+		System.out.println("2");
+
+	}
+	private void method2() {
+		System.out.println("4");
+
+	}
+	
+
+}
